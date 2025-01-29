07265038ac00bec7d9e26e42b7c271f263d1cdaf
Jonyrewind <jonyrewind@gmail.com>
2025-01-29 20:09:57 +0100

    Stashed

--- data-otservbr-global/scripts/custom/teleport_cube.lua
+++ data-otservbr-global/scripts/custom/teleport_cube.lua
@@ -3,8 +3,29 @@ local config = {
 	{ name = "Temple", position = Position(32369, 32241, 7) },
 	{ name = "NPC Room", position = Position(994, 1030, 6) },
 	{ name = "Trainer", position = Position(1116, 1094, 7) },
+<<<<<<< Updated upstream
     { name = "Citys" },
     { name = "Bosses" },
+=======
+    { name="Thais", position = Position(32369, 32241, 7) },
+    { name="Edron", position = Position(33217, 31814, 8) },
+    { name="Carlin", position = Position(32360, 31782, 7) },
+    { name="Venore", position = Position(32957, 32076, 7) },
+    { name="Ab'Dendriel", position = Position(32732, 31634, 7) },
+    { name="Port Hope", position = Position(32594, 32745, 7) },
+    { name="Yalahar", position = Position(32787, 31276, 7) },
+    { name="Kazordoon", position = Position(32649, 31925, 11) },
+    { name="Darashia", position = Position(33213, 32454, 1) },
+    { name="Rathleton", position = Position(33594, 31899, 6) },
+    { name="Svargrond", position = Position(32212, 31132, 7) },
+    { name="Farmine", position = Position(33023, 31521, 11) },
+    { name="Ankrahmun", position = Position(33194, 32853, 8) },
+    { name="Liberty Bay", position = Position(32317, 32826, 7) },
+    { name="Roshamuul", position = Position(33513, 32363, 6) },
+    { name="Gray beach", position = Position(33447, 31323, 9) },
+    { name="Issavi", position = Position(33921, 31477, 5) },
+    { name="Krailos", position = Position(33655, 31666, 8) },
+>>>>>>> Stashed changes
 }
 
 local citys = {
