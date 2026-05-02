e0cad7c0f19aab866e7ff35d68164425834afd4b
Jonyrewind <jonyrewind@gmail.com>
2026-05-02 20:55:31 +0200

    Update game_functions.hpp

--- src/lua/functions/core/game/game_functions.hpp
+++ src/lua/functions/core/game/game_functions.hpp
@@ -39,6 +39,7 @@ private:
 
 	static int luaGameGetGameState(lua_State* L);
 	static int luaGameSetGameState(lua_State* L);
+	static int luaGameGlobalServerSave(lua_State* L);
 
 	static int luaGameGetWorldType(lua_State* L);
 	static int luaGameSetWorldType(lua_State* L);
